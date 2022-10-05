fa8842386eee2fb64353db5781b26f7375d7cf79
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-06 01:40:30 +0200

    Cleaned up the code. Using WebPack to bundle!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
+import _ from "lodash";
+
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
+  // Lodash is now imported on top of this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  // For safety, remove "main" and add "private: true" in package.json
-  // This is not the best way to do this. Not immediately visible that it requires an external library (lodash)
-  // If a dependency is missing or wrong order - BOOM
-  // If it's included and not used - waste of bandwidth
+
   return element;
 }
 
