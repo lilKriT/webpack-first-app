d7a3d178788944703b7a9dd02fa3ae93c628c8b3
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-06 04:28:47 +0200

    Added an image.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import "./style.css"; // this is how you add style
+import Icon from "./icon.png"; // importing images
 
 function component() {
   const element = document.createElement("div");
@@ -7,6 +8,12 @@ function component() {
   // Lodash is now imported on top of this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  // Adding an image
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
