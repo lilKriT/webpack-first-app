2c631f0f403aa1f5a67c95f120528aba2b6c9426
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-06 15:33:34 +0200

    Added cleaning the /dist. Added dynamic bundle names.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import _ from "lodash";
-import "./style.css"; // this is how you add style
-import Icon from "./icon.png"; // importing images
-import Data from "./data.xml";
-import Notes from "./data.csv";
+// import "./style.css"; // this is how you add style
+// import Icon from "./icon.png"; // importing images
+// import Data from "./data.xml";
+// import Notes from "./data.csv";
+import printMe from "./print";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
   // Lodash is now imported on top of this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  //   element.classList.add("hello");
 
   // Adding an image
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  //   const myIcon = new Image();
+  //   myIcon.src = Icon;
 
-  element.appendChild(myIcon);
+  //   element.appendChild(myIcon);
 
   // Reading the data files:
-  console.log(Data);
-  console.log(Notes);
+  //   console.log(Data);
+  //   console.log(Notes);
+
+  btn.innerHTML = "Click me and check the console";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,45 @@
 const path = require("path");
+// This is so index can import dynamic file names
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 // This is a config I created
 module.exports = {
   // Entry point to the app
-  entry: "./src/index.js",
+  entry: { index: "./src/index.js", print: "./src/print.js" }, // this way there's multiple entry points
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   output: {
-    filename: "bundle.js", // main.js is default
+    // this will generate multitple outputs
+    filename: "[name].bundle.js", // main.js is default
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   // This is for adding css
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-    ],
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       use: ["style-loader", "css-loader"],
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  //       type: "asset/resource",
+  //     },
+  //     {
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/i,
+  //       type: "asset/resource",
+  //     },
+  //     {
+  //       test: /\.(csv|tsv)$/i,
+  //       use: ["csv-loader"],
+  //     },
+  //     {
+  //       test: /\.xml$/i,
+  //       use: ["xml-loader"],
+  //     },
+  //   ],
+  // },
 };
