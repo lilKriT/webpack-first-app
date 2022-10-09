5fe1ac5668006f6c605ec4425ab2245a43e914c1
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-09 22:47:44 +0200

    Added HMR for css.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import Print from "./print";
+import "./style.css";
 
 async function getComponent() {
   const element = document.createElement("div");
   const { default: _ } = await import("lodash");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "webpacka"], " ");
   element.onclick = Print.bind(null, "Hello webpack!");
 
   return element;
 }
 
+let element;
 getComponent().then((component) => {
-  document.body.appendChild(component);
+  element = component;
+  document.body.appendChild(element);
 });
 
 if (module.hot) {
   module.hot.accept("./print.js", () => {
     console.log("Accepting the updated module!");
-    printMe();
+    // print();
+    document.body.removeChild(element);
+    element = getComponent();
+    document.body.appendChild(element);
   });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = (env) => {
       static: "./dist",
       hot: true,
     },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
     plugins: [
       new HtmlWebpackPlugin({
         title: "Hot Module Replacement",
